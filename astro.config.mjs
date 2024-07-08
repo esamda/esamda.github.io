7aa258fd6490eaf0cd935c7f75a80b2b4ba0524d
Alden <63831735+AldenizenMC@users.noreply.github.com>
2024-07-08 13:55:34 +0700

    feat: add astro imagetools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import icon from "astro-icon";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), icon()]
+    integrations: [tailwind(), mdx(), icon(), astroImageTools()],
 });
