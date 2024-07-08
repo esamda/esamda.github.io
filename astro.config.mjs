240de48a5105c976b25c349c216dbd52bc9e172b
Alden <63831735+AldenizenMC@users.noreply.github.com>
2024-07-08 14:08:44 +0700

    feat: remove astro imagetools

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
-import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx(), icon(), astroImageTools()],
+    integrations: [tailwind(), mdx(), icon()],
 });
