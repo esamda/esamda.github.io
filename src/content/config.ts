972953410670c29a8056844d5732ebf97c197db8
Alden <63831735+AldenizenMC@users.noreply.github.com>
2024-07-09 16:35:45 +0700

    feat: replace static og:description with dynamic extraction of first 247 characters from article content
    - Removed static description from schema
    - Replaced og:description with dynamically extracted first 247 characters from the article body

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,6 @@ const articles = defineCollection({
     // Type-check frontmatter using a schema
     schema: z.object({
         title: z.string(),
-        description: z.string().optional(),
         date: z.coerce.date(),
         cover_image: z.preprocess((i) => formatPathOrUrl(import.meta.env.BASE_URL, String(i)), z.string()),
     }),
