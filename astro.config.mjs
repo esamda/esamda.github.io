95e869f94c38560cdd0d87fba59ca728908dee4c
Alden <63831735+AldenizenMC@users.noreply.github.com>
2024-07-09 12:19:07 +0700

    feat: add astro pagefind to serve previously prebuilt search index in astro dev mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
     site: "http://localhost:4321/",
     base: "/",
-    integrations: [tailwind(), mdx(), icon()],
+    integrations: [tailwind(), mdx(), icon(), pagefind()],
 });
