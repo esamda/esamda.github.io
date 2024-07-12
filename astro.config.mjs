dd3adc568f6fc3ce0b72596ddbee1ea2c6aeee93
Alden <63831735+AldenizenMC@users.noreply.github.com>
2024-07-12 10:36:21 +0700

    feat: add astro markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import pagefind from "astro-pagefind";
+import markdownIntegration from "@astropub/md";
 
 // https://astro.build/config
 export default defineConfig({
     site: "http://localhost:4321/",
     base: "/",
-    integrations: [tailwind(), mdx(), icon(), pagefind()],
+    integrations: [tailwind(), mdx(), icon(), pagefind(), markdownIntegration()],
 });
